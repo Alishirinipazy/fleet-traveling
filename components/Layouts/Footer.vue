c2138e8d84515db5426f27246d16e9dab278f73e
shirinipaz <alishirinipazy@gmail.com>
2025-01-08 14:26:09 +0330

    start footer

--- components/Layouts/Footer.vue
+++ components/Layouts/Footer.vue
@@ -1,32 +1,60 @@
 <template>
-  <div class="container mx-auto mt-5">
-    <h1 class="text-center text-4xl mb-4">سلام، Tailwind CSS و Bootstrap!</h1>
-    <button class="btn btn-primary mb-2">دکمه Bootstrap</button>
-    <div class="flex items-center justify-center h-48 bg-blue-500">
-      <p class="text-white">این یک بلاک از Tailwind CSS است!</p>
-    </div>
-  </div>
-  <!-- Button trigger modal -->
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Launch demo modal
-  </button>
+  <footer class="container  p-4">
+    <div class="row p-4">
+      <div class="col-lg-4 ">
+        <img src="@/public/images/global/brand.png" height="36" width="36" :alt="$t('brand')">
+        {{ $t('brand') }}
+        <p class="py-2">{{ $t('description') }}</p>
+      </div>
+      <div class="col-lg-2">
+        <ul class="list-unstyled">
+          <li class="py-2">
+            <nuxt-link class="link-underline-light text-color mx-3" to="/">{{ $t('landing.footer.about') }}</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link class="link-underline-light text-color mx-3" to="/">{{ $t('landing.footer.contact') }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-2">
+        <ul class="list-unstyled">
+          <li class="py-2">
+            <nuxt-link class="link-underline-light text-color mx-3" to="/">{{
+                $t('landing.header.nav.option.otn1')
+              }}
+            </nuxt-link>
+          </li>
+          <li >
+            <nuxt-link class="link-underline-light text-color mx-3" to="/">{{
+                $t('landing.header.nav.option.otn2')
+              }}
+            </nuxt-link>
+          </li>
+          <li class="py-2">
+            <nuxt-link class="link-underline-light text-color mx-3" to="/">{{
+                $t('landing.header.nav.option.otn3')
+              }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-4">
+        <div class="input-group mb-3 position-relative">
+          <input type="text" class="form-control rounded-4" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+          <button class="btn btn-outline-primary footer-btn-send rounded-circle" type="button" id="button-addon2"></button>
+        </div>
 
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
       </div>
     </div>
-  </div>
+  </footer>
 </template>
+<style scoped>
+.footer-btn-send {
+  position: absolute;
+  left: 0;
+
+}
+</style>
+<script setup lang="ts">
+</script>
