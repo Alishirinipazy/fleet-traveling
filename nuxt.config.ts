cdf2814a8f9df338e243c22d48851bf40932c580
shirinipaz <alishirinipazy@gmail.com>
2025-01-17 15:23:02 +0330

    deploing change from git

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,7 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
     css: ['~/assets/css/main.css'],
-    modules: [
-        '@primevue/nuxt-module'
-    ],
+    modules: ['@primevue/nuxt-module', '@nuxthub/core'],
     primevue: {
         usePrimeVue: true,
         options: {
